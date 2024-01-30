ea5c326fc02a16ccbee3cb2c4c2693bf72ed4e06
Your Name01~ <you@example.com>
2024-01-30 03:06:59 -0800

    first commit

---
+++ images/show.js
@@ -0,0 +1,8 @@
+function show() {
+  var x = document.getElementsByClassName("collapse")[0];
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
